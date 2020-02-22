1f439d4f1441ce95abcc4611abad75d99fe572d6
VarianM <mizeyuyu@gmail.com>
2020-02-22 18:51:21 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 yarn build &&
 git checkout gh-pages &&
-rm -rf *.html *.css *.js *.json ;
+rm -rf *.html *.css *.js *.json &&
 mv dist/* ./ &&
-rm -rf dist &&
+rm -rf dist ;
 git add . &&
 git commit -m "update deploy" &&
-git push &&
-git checkout master
+git push
